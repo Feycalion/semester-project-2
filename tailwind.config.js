52097d9242b150565fe7f7f71fe075375c8bf2bf
Feycalion <anjakves@gmail.com>
2024-05-12 16:11:38 +0200

    Refactor header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./*.html"],
   theme: {
+    colors: {
+      background: "#FFFAF0",
+      offwhite: "#FFEEDE",
+      lightgray: "#484845",
+      darkgray: "#1C1C1B",
+    },
+    fontFamily: {
+      poiret: ["Poiret One, sans-serif"],
+      montserrat: ["Montserrat, sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
